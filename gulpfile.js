9997ec74d236a0de7a64e1ac2c3afd0332c0b378
Josebaml <josebaml@gmail.com>
2020-02-06 10:30:27 +0100

    Versión completa frameworkCSS

--- gulpfile.js
+++ gulpfile.js
@@ -13,41 +13,41 @@ var newer = require('gulp-newer');
 // Uso de browserSync
 gulp.task('default', function(){
     browserSync.init({
-        server: "./app"
+        server: "./assets"
     });
     gulp.watch("./scss/**/*.scss", gulp.series("sass"));
-    gulp.watch("./app/css/*.css", gulp.series("cssnano"));
-    gulp.watch("./app/*.html", gulp.series("minify"));
-    gulp.watch("./app/*.html").on('change', browserSync.reload);
+    gulp.watch("./assets/css/*.css", gulp.series("cssnano"));
+    gulp.watch("./assets/*.html", gulp.series("minify"));
+    gulp.watch("./assets/*.html").on('change', browserSync.reload);
 })
 
 // Compile sass into CSS & auto-inject into browsers with browserSync
 gulp.task('sass', function(){
     return gulp.src('scss/**/*.scss')
         .pipe(plumber())
-        .pipe(newer('app/css'))
+        .pipe(newer('assets/css'))
         .pipe(sass())
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 2 versions'],
             cascade: false,
             grid: "autoplace"
         }))
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest('assets/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('cssnano', function(){
-    return gulp.src('./app/css/*.css')
-        .pipe(newer('app/dist/css'))
+    return gulp.src('./assets/css/*.css')
+        .pipe(newer('assets/dist/css'))
         .pipe(cssnano())
-        .pipe(gulp.dest('app/public/css'));
+        .pipe(gulp.dest('assets/public/css'));
 });
 
 gulp.task('imagenes', function(){
-    gulp.src('./app/img/*')
-        .pipe(newer('./app/dist/img'))
+    gulp.src('./assets/img/*')
+        .pipe(newer('./assets/dist/img'))
         .pipe(imagemin())
-        .pipe(gulp.dest('./app/public/img'))
+        .pipe(gulp.dest('./assets/public/img'))
 });
 
 //gulp.task('autoprefixer', function(){
@@ -60,8 +60,8 @@ gulp.task('imagenes', function(){
 //});
 
 gulp.task('minify', function(){
-    return gulp.src('./app/*.html')
-      .pipe(newer('./app/dist'))
+    return gulp.src('./assets/*.html')
+      .pipe(newer('./assets/dist'))
       .pipe(htmlmin({ collapseWhitespace: true }))
-      .pipe(gulp.dest('./app/public'));
+      .pipe(gulp.dest('./assets/public'));
 });
